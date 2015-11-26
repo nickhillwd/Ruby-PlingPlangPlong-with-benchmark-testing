46fdf7a1e6a03787cd1946a40a21dbd33dd64263
nickhillwd <nickhillwd@gmail.com>
2015-11-26 11:32:01 +0000

    version running using a for loop

--- Pling_plang_plong.rb
+++ Pling_plang_plong.rb
@@ -3,15 +3,14 @@
 #plong = 7
 
 #up to 105
-i = 1
 for i in 1..105
-  if i % 3 && 5 && 7 == 0
+  if (i % 3 == 0) && (i % 5 == 0) && (i % 7 == 0)
     puts "plingPlangPlong"
-  elsif i % 3 && 5 == 0
+  elsif (i % 3 == 0) && (i % 5 == 0)
     puts "PlingPlang"
   elsif i %  5 == 0
     puts "plang"
-  elsif i i % 3 == 0
+  elsif i % 3 == 0
     puts "pling"
   else
     puts i
