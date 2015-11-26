0224334ea0f1eaa847b8674be4d738f173ada3f4
nickhillwd <nickhillwd@gmail.com>
2015-11-26 11:54:59 +0000

    removed whitespace in a desperate bid for efficiency

--- Pling_plang_plong.rb
+++ Pling_plang_plong.rb
@@ -1,18 +1,13 @@
-#pling = 3
-#plang = 5
-#plong = 7
-
-#up to 105
 for i in 1..105
-  if (i % 3 == 0) && (i % 5 == 0) && (i % 7 == 0)
-    print "PlingPlangPlong"
-  elsif (i % 3 == 0) && (i % 5 == 0)
-    print "PlingPlang, "
-  elsif i %  5 == 0
-    print "Plang, "
-  elsif i % 3 == 0
-    print "Pling, "
+  if (i%3==0)&&(i%5==0)&&(i%7==0)
+    print"PlingPlangPlong"
+  elsif (i%3==0)&&(i%5==0)
+    print"PlingPlang, "
+  elsif i%5==0
+    print"Plang, "
+  elsif i%3==0
+    print"Pling, "
   else
-    print "#{i.to_s}, "
+    print"#{i}, "
   end
 end
